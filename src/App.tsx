ba481af659c86b25ccb9222d106d3bc7fa89de04
bartwroblewski <barti.wroblewski@gmail.com>
2020-10-05 20:33:00 +0200

    Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import MultiSelect from './components/MultiSelect'
+import NewItemModal from './components/NewItemModal'
 
 const App = () => {
 
+  const [modalVisible, setModalVisible] = React.useState<boolean>(false)
+
+  const toggleModal = () => {
+    setModalVisible(prev => !prev)
+  }
+
+  React.useEffect(() => {
+    console.log(modalVisible)
+  }, [modalVisible])
+
   return (
-    <MultiSelect 
-      options={['aaaa', 'bbbb', 'cccccc', 'fdaaf', 'aaaaaaaaaaaaaaaaaaaa', 'v', 'vdv', 'vwe']} 
-      initial_option="Choose an option..."
-    />
+    <div>
+      {modalVisible 
+        ? <NewItemModal />
+        : null
+      }
+      <button onClick={toggleModal}>Toggle modal</button>
+    </div>
   );
 }
 
