40d6f37a08383b7b20e8a364c7b834b5416b65e6
bartwroblewski <barti.wroblewski@gmail.com>
2020-12-10 16:13:08 +0100

    Play once

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import * as Tone from 'tone'
 import './App.css'
 import { Sound, SoundSelect } from './components/SoundSelect'
 
-type SeqCol = Sound[]
-
-type Seq = SeqCol[]
-
 const synth = new Tone.Synth().toDestination()
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
@@ -16,98 +12,69 @@ const INTERVAL = ((60 / BPM) / 4) * 1000 // 16th notes
 const DIVISION = 16 // 16th notes
 const LOOP_LENGTH = INTERVAL * (DIVISION + 2) // why + 2?
 
+type Cell = 0 | 1
+type Row = Array<Cell>
+type Grid = Array<Row>
+
 const App = () => {
 
-  const getEmptySeq = (): Seq => {
-    const seq: Seq = []
-    for (let i=0; i<DIVISION; i++) {
-      const col: SeqCol = [
-        {pitch: '', length: ''},
-        {pitch: '', length: ''},
-        {pitch: '', length: ''},
-        {pitch: '', length: ''},
-      ]
-      seq.push(col)
+  const COLS = 16
+
+  const [grid, setGrid] = React.useState<Grid>([])
+
+  const getEmptyRow = () => {
+    let row: Row = []
+    for (let i=0; i<COLS; i++) {
+      const cell: Cell = 0
+      row.push(cell)
     }
-    return seq
+    return row
   }
 
-  const [seq, setSeq] = React.useState<Seq>(getEmptySeq())
-
-  const [sounds, setSounds] = React.useState([
-    {pitch: "C3", length: "16n"},
-    {pitch: "D4", length: "16n"},
-    {pitch: "E2", length: "16n"},
-    {pitch: "F4", length: "16n"},
-  ])
-
-  const placeSound = (col: number, row: number) => {
-      const newSeq: Seq = [...seq]
-      newSeq[col][row] = sounds[row]
-      setSeq(newSeq)
+  const addRow = () => {
+    const emptyRow = getEmptyRow()
+    setGrid([...grid, emptyRow])
   }
 
-  const removeSample = (col: number, row: number) => {
-    const newSeq: Seq = [...seq]
-    newSeq[col][row] = {pitch: '', length: ''}
-    setSeq(newSeq)
+  const enableCell = (row: number, col: number) => {
+    const newGrid = [...grid]
+    newGrid[row][col] = 1
+    setGrid(newGrid)
+  }
+
+  const disableCell = (row: number, col: number) => {
+    const newGrid = [...grid]
+    newGrid[row][col] = 0
+    setGrid(newGrid)
   }
 
   const playSound = (sound: Sound) => {
-    if (sound.pitch) {
       synth.triggerAttackRelease(sound.pitch, sound.length)
-    }
   }
 
-  const handleContextMenu = (e: any, rowIndex: number, colIndex: number) => {
-    e.preventDefault()
-    removeSample(colIndex, rowIndex)
+  const playGridOnce = () => {
+    const rows = grid.length
+    for (let col=0; col<COLS; col++) {
+      for (let row=0; row<rows; row++) {
+        const cell = grid[row][col]
+        if (cell) {
+          console.log('playing', row, col)
+        }
+      }      
+    } 
   }
 
-  const playSeq = async() => {
-    for (let col of seq) {
-      await sleep(INTERVAL)
-      for (let sound of col) {
-        playSound(sound)
-      }
-    }
-  }
-
-  const loop = () => setInterval(playSeq, LOOP_LENGTH)
-
-  const soundSelects = sounds.map(sound => {
-    return (
-      <SoundSelect
-        sounds={sounds}
-        setSounds={setSounds}
-      />
-    )
-    })
-
-  const grid = seq.map((col, colIndex) => {
-    const sounds = col.map((sound, rowIndex) => {
-      return (
-        sound.pitch
-          ? <div onClick={(e) => playSound(sound)} onContextMenu={e => handleContextMenu(e, rowIndex, colIndex)} className='sample filled'></div>
-          : <div onClick={() => placeSound(colIndex, rowIndex)} className='sample empty'></div>
-      )
-    })
-    return (
-      <div className='grid'>
-        {sounds}
-      </div>
-    )
-  })
-
-  React.useEffect(() => console.log(sounds), [sounds])
+  React.useEffect(() => console.log(grid), [grid])
 
   return (
     <div>
-      {soundSelects}
-      {grid}
-      <button type="button" onClick={loop}>Play</button>
+      {grid.length}
+      <button onClick={addRow}>Add row</button>
+      <button onClick={() => enableCell(1, 5)}>Enable cell 1, 5</button>
+      <button onClick={() => disableCell(1, 5)}>Disable cell 1, 5</button>
+      <button onClick={playGridOnce}>Play grid once</button>
     </div>
   )
 }
 
-export default App;
+export default App
