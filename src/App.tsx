1088c515252a1f75d126213787e48f3d4c0d732a
bartwroblewski <barti.wroblewski@gmail.com>
2020-12-13 15:00:11 +0100

    redo

--- src/App.tsx
+++ src/App.tsx
@@ -99,25 +99,31 @@ const App = () => {
 
 }
 
-const GridCell = ({row, col}: {row?: number, col?: number}) => {
+const GridCell = ({row, col}: {row: number, col: number}) => {
+
+  const handleClick = (e: any) => console.log(row, col)
+
   return (
-    <div className="grid-cell"></div>
+    <div 
+      className="grid-cell"
+      onClick={handleClick}
+    ></div>
   )
 }
 
-const GridStep = ({col}: {col?: number}) => 
+const GridStep = ({col}: {col: number}) => 
   <div className="grid-step">
+        <GridCell row={0} col={col} />
         <GridCell row={1} col={col} />
         <GridCell row={2} col={col} />
         <GridCell row={3} col={col} />
-        <GridCell row={4} col={col} />
   </div>
 
 const Grid = () => { 
 
   const steps = []
   for (let i=0; i<16; i++) {
-    steps.push(<GridStep col={i} />)
+    steps.push(<GridStep key={i} col={i} />)
   }
   
   return (
