ca16fa796f5808f4e22902d455c9f06b630ddda7
bartwroblewski <barti.wroblewski@gmail.com>
2020-12-14 21:35:33 +0100

    reduce

---
+++ src/components/Test.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Sample = {
+    url: string,
+    volume: number,
+}
+
+type Col = Sample[]
+
+type Seq = Col[]
+
+const col: Col = []
+
+const Test = () => <div></div>
+
+export default Test
