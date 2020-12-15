b048db514f59449fd80c2de5a5d48a61b106d545
bartwroblewski <barti.wroblewski@gmail.com>
2020-12-15 21:16:35 +0100

    selects

--- src/App.tsx
+++ src/App.tsx
@@ -22,14 +22,24 @@ import { Sequences } from './sequencer'
       sequencer.addSequence()
       setSequences([...sequencer.sequences])
     }
-
+    
+    const soundSelects = sequences.map(sequence =>
+      <select className="sound-selects">
+        {sounds.map(sound => <option>{sound[0]}</option>)}
+      </select>
+    )
     return (
       <div>
         <button onClick={handleAddRow}>Add row</button>
         <button onClick={sequencer.play.bind(sequencer)}>Play</button>
         <button onClick={sequencer.loop.bind(sequencer)}>Loop</button>
         <button onClick={sequencer.stopLoop.bind(sequencer)}>Stop loop</button>
-        <Grid sequences={sequences} onCellClick={handleCellClick}/>
+        <div className="sequencer">
+          <div>
+            {soundSelects}
+          </div>
+          <Grid sequences={sequences} onCellClick={handleCellClick}/>
+        </div>
       </div>
     )
 }
