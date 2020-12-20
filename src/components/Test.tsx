2ff385238dfb35f0f724944b42e3a1969237479d
bartwroblewski <barti.wroblewski@gmail.com>
2020-12-20 11:31:05 +0100

    'reducers'

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -2,19 +2,42 @@ interface Action {
     type: string,
 }
 
-const arr = [1, 2, 3, 4]
+interface GUIHandlers {
+    onAdd: () => number[]
+}
 
-const reducer = (state: number[], action: Action): any => {
-    switch (action.type) {
-        case 'ADD':
-            return state.concat(state[state.length -1] + 1)
+const App = () => {
+
+    let arr = [1, 2, 3, 4]
+
+    const reducer = (state: number[], action: Action): any => {
+        switch (action.type) {
+            case 'ADD':
+                return state.concat(state[state.length -1] + 1)
+        }
+    }
+
+    const add = (): number[] => arr = reducer(arr, {type: 'ADD'})
+
+    return {
+        arr: arr,
+        handleAdd: add,
     }
 }
 
-let newArr = reducer(arr, {type: 'ADD'})
-newArr = reducer(newArr, {type: 'ADD'})
-newArr = reducer(newArr, {type: 'ADD'})
-newArr = reducer(newArr, {type: 'ADD'})
+const app = App()
+
+// GUI
+const GUIHandlers = {onAdd: app.handleAdd} 
+const AppGUI = (handlers: GUIHandlers): any => {
+    return {add: handlers.onAdd}
+}
+const gui = AppGUI(GUIHandlers)
+
+// simulate GUI action
+let newArr = gui.add()
+newArr = gui.add()
+newArr = gui.add()
 console.log(newArr)
 
 export {}
