6fcc920210fa236c0b3dd4d5f33a93341749852c
bartwroblewski <barti.wroblewski@gmail.com>
2021-02-03 14:51:57 +0100

    def sound

--- src/app/Sound.ts
+++ src/app/Sound.ts
@@ -3,7 +3,7 @@ import * as Tone from 'tone'
 const synth = new Tone.Synth().toDestination()
 
 export type Sound = [string, string]
-export const defaultSound = {name: 'D', pitch: 4}
+export const defaultSound = {name: 'C', pitch: 4}
 export const defaultNoteLength: string = '16N'
 export const soundNames: string[] = [
     'C', 'C#',
